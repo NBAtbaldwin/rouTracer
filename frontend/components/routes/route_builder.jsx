d2646ebc9d3531b9e097dd974ed022b782b48deb
Tyler Baldwin <tzadoroznybaldwin@gmail.com>
2018-09-14 11:30:42 -0400

    enable geolocation

--- frontend/components/routes/route_builder.jsx
+++ frontend/components/routes/route_builder.jsx
@@ -16,8 +16,27 @@ class RouteBuilder extends React.Component {
       },
       zoom: 13
     };
+    let infoWindow;
     this.map = new google.maps.Map(this.mapNode, mapOptions);
-  }
+    infoWindow = new google.maps.InfoWindow;
+    const that = this;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        debugger
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Current position.');
+        infoWindow.open(that.map);
+        that.map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, infoWindow, that.map.getCenter())
+      })} else {
+        handleLocationError(false, infoWindow, this.map.getCenter());
+      }
+    };
 
   render() {
     return (
