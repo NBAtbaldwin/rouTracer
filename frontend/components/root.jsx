015e2c50f04b944584932718f3c5582993c54495
Tyler Baldwin <tzadoroznybaldwin@gmail.com>
2018-12-12 10:47:40 -0500

    bugfix for page position in router

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
+import ScrollToTop from './scroll_top';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </HashRouter>
   </Provider>
 );
